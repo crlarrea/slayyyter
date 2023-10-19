36f729649cd10c83fc3f70c02f58a9a2a4aed077
Long Lane <tech@long-lane.co.uk>
2023-10-19 23:35:33 +0100

    add mobile responsivenss

--- src/components/Vinyls.jsx
+++ src/components/Vinyls.jsx
@@ -11,36 +11,42 @@ export const Vinyls = () => {
       <article>
         <h2>Albums</h2>
       </article>
-      {albums.map((entry) => {
-        return (
-          <article
-            key={`album-${entry.name}`}
-            onClick={() => {
-              updateAlbum(entry);
-            }}
-          >
-            <img src={entry.image} />
-            {/* <p>{entry.name}</p> */}
-          </article>
-        );
-      })}
-      {currentSelection && (
-        <article id="album-description">
-          <h3>{currentSelection.name}</h3>
-          <span>
-            {Intl.NumberFormat("en-US", {
-              style: "currency",
-              currency: "USD",
-            }).format(currentSelection.price)}
-          </span>
-          <ol>
-            {currentSelection.tracks.map((track, index) => {
-              return <li key={`${currentSelection.name}-${track}`}>{track}</li>;
-            })}
-          </ol>
-          <a href="#">buy</a>
-        </article>
-      )}
+      <article>
+        {albums.map((entry) => {
+          return (
+            <div
+              key={`album-${entry.name}`}
+              onClick={() => {
+                updateAlbum(entry);
+              }}
+            >
+              <img src={entry.image} />
+              {/* <p>{entry.name}</p> */}
+            </div>
+          );
+        })}
+      </article>
+      <article id="album-description">
+        {currentSelection && (
+          <>
+            <h3>{currentSelection.name}</h3>
+            <span>
+              {Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(currentSelection.price)}
+            </span>
+            <ol>
+              {currentSelection.tracks.map((track, index) => {
+                return (
+                  <li key={`${currentSelection.name}-${track}`}>{track}</li>
+                );
+              })}
+            </ol>
+            <a href="#">buy</a>
+          </>
+        )}{" "}
+      </article>
     </section>
   );
 };
